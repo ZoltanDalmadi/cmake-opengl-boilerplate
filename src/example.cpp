0178acc70002b83fdaa55dd3a976aeecafa45d0b
Zoltan Dalmadi <dmz985@gmail.com>
2016-11-09 11:46:00 +0100

    Reindented example

--- src/example.cpp
+++ src/example.cpp
@@ -13,7 +13,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 
 	if ((key == GLFW_KEY_S) && action == GLFW_PRESS) {
 		SOIL_save_screenshot(
-				"awesomenessity.bmp", SOIL_SAVE_TYPE_BMP, 0, 0, WIDTH, HEIGHT);
+			"awesomenessity.bmp", SOIL_SAVE_TYPE_BMP, 0, 0, WIDTH, HEIGHT);
 	}
 }
 
@@ -42,10 +42,10 @@ int main() {
 	}
 
 	// Print OpenGL information
-	std::cout << "Vendor: " << glGetString(GL_VENDOR) << '\n'
-	          << "Renderer: " << glGetString(GL_RENDERER) << '\n'
-	          << "OpenGL: " << glGetString(GL_VERSION) << '\n'
-	          << "GLSL: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
+	std::cout << "Vendor: "   << glGetString(GL_VENDOR)                   << '\n'
+	          << "Renderer: " << glGetString(GL_RENDERER)                 << '\n'
+	          << "OpenGL: "   << glGetString(GL_VERSION)                  << '\n'
+	          << "GLSL: "     << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
 
 	// set window background to a nice blueish color
 	glClearColor(0.275f, 0.510f, 0.706f, 1.0f);
